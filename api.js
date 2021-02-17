02d7b7fb753b8b3db0b0ef4a3e7f22f846d7ba9c
개발자 <gaebalja@gaebaljaui-MacBookPro.local>
2021-02-17 23:56:07 +0900

    20210217

--- api.js
+++ api.js
@@ -2,11 +2,13 @@ const fetch = require('node-fetch');
 const express = require('express');
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : '',
+  host     : '35.223.127.108',
   user     : 'root',
   password : '1111',
-  database : 'mysql'
+  database : 'weather_information'
 });
+
+connection.connect(); 
 
 const app = express();
 
@@ -55,9 +57,10 @@ console.log(data)
 */
 // async로 호출api를 기다리지 않으면 data엔 아무런 값도 들어가지 않는다 async await 사용
 
+
 app.get('/',(req,res) => {
     var location = req.query.location;
-
+    
     (async () => {
         var data;
         async function getApi(){
@@ -68,25 +71,33 @@ app.get('/',(req,res) => {
         await getApi()
         if (data.cod == 200) { // 검색이 가능할 경우
 
-            connection.connect();
- 
-            connection.query('select * from ', function (error, results, fields) {
+                   
+            
+            var sql = 'insert into json_weather(w_data) values(\'' + JSON.stringify(data) + '\')'
+
+            // JSON형식의 data의 내용을 알고싶다면 JSON.stringify(data)를 사용한다.
+
+            connection.query(sql, function (error, results, fields) {
                 if (error) {
                     console.log(error);
                 } else {
                     console.log(results);
                 }
             });
-            
-            connection.end();
 
             return res.json(data)
         } else if (data.cod == 404) { // 검색이 불가능할 경우
             return res.json(data)
+        } else {
+            return "doyeon"
         }
     })();
 })
 
+
+
 app.listen(3001, function(){
     console.log("Express server has started on port 3001")
 })
+
+// mysql connect와 end위치를 정해야한다.
